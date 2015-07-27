460555715b1c2fc708909ad89f0a31df1b01a62c
Sander van der Burg <svanderburg@gmail.com>
2015-07-27 20:44:15 +0000

    Add Hydra expression

---
+++ release.nix
@@ -0,0 +1,79 @@
+{ nixpkgs ? <nixpkgs>
+, disnix_stafftracker_nodejs_example ? {outPath = ./.; rev = 1234;}
+, officialRelease ? false
+, systems ? [ "i686-linux" "x86_64-linux" ]
+}:
+
+let
+  pkgs = import nixpkgs {};
+  
+  jobs = rec {
+    tarball =
+      let
+        pkgs = import nixpkgs {};
+  
+        disnixos = import "${pkgs.disnixos}/share/disnixos/testing.nix" {
+          inherit nixpkgs;
+        };
+      in
+      disnixos.sourceTarball {
+        name = "disnix-stafftracker-nodejs-example-tarball";
+        version = builtins.readFile ./version;
+        src = disnix_stafftracker_nodejs_example;
+        inherit officialRelease;
+      };
+      
+    build =
+      pkgs.lib.genAttrs systems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+  
+          disnixos = import "${pkgs.disnixos}/share/disnixos/testing.nix" {
+            inherit nixpkgs system;
+        };
+        in
+        disnixos.buildManifest {
+          name = "disnix-stafftracker-nodejs-example";
+          version = builtins.readFile ./version;
+          inherit tarball;
+          servicesFile = "deployment/DistributedDeployment/services.nix";
+          networkFile = "deployment/DistributedDeployment/network.nix";
+          distributionFile = "deployment/DistributedDeployment/distribution.nix";
+        }
+      );
+    tests = 
+      let
+        disnixos = import "${pkgs.disnixos}/share/disnixos/testing.nix" {
+          inherit nixpkgs;
+        };
+      in
+      disnixos.disnixTest {
+        name = "disnix-stafftracker-nodejs-example-tests";
+        inherit tarball;
+        manifest = builtins.getAttr (builtins.currentSystem) build;
+        networkFile = "deployment/DistributedDeployment/network.nix";
+        testScript =
+          ''
+            # Wait for a while and capture the output of the entry page
+            my $result = $test3->mustSucceed("sleep 30; curl --fail http://test1/stafftracker");
+            
+            # The entry page should contain my name :-)
+            
+            if ($result =~ /Sander/) {
+                print "Entry page contains Sander!\n";
+            }
+            else {
+                die "Entry page should contain Sander!\n";
+            }
+            
+            # Start Firefox and take a screenshot
+            
+            $test3->mustSucceed("firefox http://test1/stafftracker &");
+            $test3->waitForWindow(qr/Nightly/);
+            $test3->mustSucceed("sleep 30");
+            $test3->screenshot("screen");
+          '';
+      };
+  };
+in
+jobs
