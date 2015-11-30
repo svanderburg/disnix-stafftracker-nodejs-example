392d83bcc4282a32b7238dd9c8fb639830a95918
Sander van der Burg <svanderburg@gmail.com>
2015-11-30 12:06:01 +0000

    Add build and test job for the caching enabled variant

--- release.nix
+++ release.nix
@@ -23,8 +23,8 @@ let
         inherit officialRelease;
       };
       
-    build =
-      pkgs.lib.genAttrs systems (system:
+    build = {
+      without_caching = pkgs.lib.genAttrs systems (system:
         let
           pkgs = import nixpkgs { inherit system; };
   
@@ -41,39 +41,94 @@ let
           distributionFile = "deployment/DistributedDeployment/distribution.nix";
         }
       );
-    tests = 
-      let
-        disnixos = import "${pkgs.disnixos}/share/disnixos/testing.nix" {
-          inherit nixpkgs;
+      
+      with_caching = pkgs.lib.genAttrs systems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+  
+          disnixos = import "${pkgs.disnixos}/share/disnixos/testing.nix" {
+            inherit nixpkgs system;
         };
-      in
-      disnixos.disnixTest {
-        name = "disnix-stafftracker-nodejs-example-tests";
-        inherit tarball;
-        manifest = builtins.getAttr (builtins.currentSystem) build;
-        networkFile = "deployment/DistributedDeployment/network.nix";
-        testScript =
-          ''
-            # Wait for a while and capture the output of the entry page
-            my $result = $test3->mustSucceed("sleep 30; curl --fail http://test1/stafftracker");
-            
-            # The entry page should contain my name :-)
-            
-            if ($result =~ /Sander/) {
-                print "Entry page contains Sander!\n";
-            }
-            else {
-                die "Entry page should contain Sander!\n";
-            }
-            
-            # Start Firefox and take a screenshot
-            
-            $test3->mustSucceed("firefox http://test1/stafftracker &");
-            $test3->waitForWindow(qr/Nightly/);
-            $test3->mustSucceed("sleep 30");
-            $test3->screenshot("screen");
-          '';
-      };
+        in
+        disnixos.buildManifest {
+          name = "disnix-stafftracker-nodejs-example";
+          version = builtins.readFile ./version;
+          inherit tarball;
+          servicesFile = "deployment/DistributedDeployment/services-with-caching.nix";
+          networkFile = "deployment/DistributedDeployment/network.nix";
+          distributionFile = "deployment/DistributedDeployment/distribution.nix";
+        }
+      );
+    };
+    
+    tests = {
+      without_caching =
+        let
+          disnixos = import "${pkgs.disnixos}/share/disnixos/testing.nix" {
+            inherit nixpkgs;
+          };
+        in
+        disnixos.disnixTest {
+          name = "disnix-stafftracker-nodejs-example-tests";
+          inherit tarball;
+          manifest = builtins.getAttr (builtins.currentSystem) (build.without_caching);
+          networkFile = "deployment/DistributedDeployment/network.nix";
+          testScript =
+            ''
+              # Wait for a while and capture the output of the entry page
+              my $result = $test3->mustSucceed("sleep 30; curl --fail http://test1/stafftracker");
+              
+              # The entry page should contain my name :-)
+              
+              if ($result =~ /Sander/) {
+                  print "Entry page contains Sander!\n";
+              }
+              else {
+                  die "Entry page should contain Sander!\n";
+              }
+              
+              # Start Firefox and take a screenshot
+              
+              $test3->mustSucceed("firefox http://test1/stafftracker &");
+              $test3->waitForWindow(qr/Nightly/);
+              $test3->mustSucceed("sleep 30");
+              $test3->screenshot("screen");
+            '';
+        };
+      with_caching =
+        let
+          disnixos = import "${pkgs.disnixos}/share/disnixos/testing.nix" {
+            inherit nixpkgs;
+          };
+        in
+        disnixos.disnixTest {
+          name = "disnix-stafftracker-nodejs-example-tests";
+          inherit tarball;
+          manifest = builtins.getAttr (builtins.currentSystem) (build.with_caching);
+          networkFile = "deployment/DistributedDeployment/network.nix";
+          testScript =
+            ''
+              # Wait for a while and capture the output of the entry page
+              my $result = $test3->mustSucceed("sleep 30; curl --fail http://test1/stafftracker");
+              
+              # The entry page should contain my name :-)
+              
+              if ($result =~ /Sander/) {
+                  print "Entry page contains Sander!\n";
+              }
+              else {
+                  die "Entry page should contain Sander!\n";
+              }
+              
+              # Start Firefox and take a screenshot
+              
+              $test3->mustSucceed("firefox http://test1/stafftracker &");
+              $test3->waitForWindow(qr/Nightly/);
+              $test3->mustSucceed("sleep 30");
+              $test3->screenshot("screen");
+            '';
+        };
+    };
   };
 in
 jobs
