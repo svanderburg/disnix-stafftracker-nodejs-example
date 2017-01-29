d828638bbcc118860ccf88f3f786052b98b928b0
Sander van der Burg <svanderburg@gmail.com>
2017-01-29 21:53:39 +0000

    Remove obsolete mongodb setting

--- deployment/configurations/test-vm-mongodb.nix
+++ deployment/configurations/test-vm-mongodb.nix
@@ -4,9 +4,6 @@
   networking.firewall.enable = false;
   services.mongodb.enable = true;
   services.mongodb.bind_ip = "0.0.0.0";
-  services.mongodb.extraConfig = ''
-    nojournal = true
-  '';
   services.disnix.enable = true;
   
   users.extraGroups = {
