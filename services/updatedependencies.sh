a1784aa25101373d54cabda870ca26bba786083f
Sander van der Burg <svanderburg@gmail.com>
2017-04-11 21:59:00 +0200

    Regenerate NPM dependencies with node2nix 1.2.0

--- services/updatedependencies.sh
+++ services/updatedependencies.sh
@@ -5,6 +5,6 @@ rm -f node-env.nix
 for i in webapplications/stafftracker webservices/{staffservice,zipcodeservice,roomservice}
 do
     cd $i
-    npm2nix -e ../../node-env.nix
+    node2nix -e ../../node-env.nix
     cd ../..
 done
