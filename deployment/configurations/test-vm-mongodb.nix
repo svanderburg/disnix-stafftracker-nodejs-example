3c0c744e383b135af49eec78a1d98e118cafe00b
Sander van der Burg <svanderburg@gmail.com>
2020-04-14 15:07:19 +0200

    Fix client test machine configuration

--- deployment/configurations/test-vm-mongodb.nix
+++ deployment/configurations/test-vm-mongodb.nix
@@ -5,11 +5,11 @@
   services.mongodb.enable = true;
   services.mongodb.bind_ip = "0.0.0.0";
   services.disnix.enable = true;
-  
+
   users.extraGroups = {
     nginx = { gid = 60; };
   };
-  
+
   users.extraUsers = {
     nginx = { group = "nginx"; uid = 60; };
   };
