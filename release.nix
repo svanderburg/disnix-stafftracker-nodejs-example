7c47b9b002f7d312804de3a524c261f2d2b16812
Sander van der Burg <svanderburg@gmail.com>
2020-09-28 22:51:24 +0200

    Move to the python test driver

--- release.nix
+++ release.nix
@@ -7,21 +7,18 @@
 let
   pkgs = import nixpkgs {};
 
-  jobs = rec {
-    tarball =
-      let
-        pkgs = import nixpkgs {};
+  disnixos = import "${pkgs.disnixos}/share/disnixos/testing.nix" {
+    inherit nixpkgs;
+  };
 
-        disnixos = import "${pkgs.disnixos}/share/disnixos/testing.nix" {
-          inherit nixpkgs;
-        };
-      in
-      disnixos.sourceTarball {
-        name = "disnix-stafftracker-nodejs-example-tarball";
-        version = builtins.readFile ./version;
-        src = disnix_stafftracker_nodejs_example;
-        inherit officialRelease;
-      };
+  version = builtins.readFile ./version;
+
+  jobs = rec {
+    tarball = disnixos.sourceTarball {
+      name = "disnix-stafftracker-nodejs-example-tarball";
+      src = disnix_stafftracker_nodejs_example;
+      inherit officialRelease version;
+    };
 
     build = {
       without_caching = pkgs.lib.genAttrs systems (system:
@@ -34,8 +31,7 @@ let
         in
         disnixos.buildManifest {
           name = "disnix-stafftracker-nodejs-example";
-          version = builtins.readFile ./version;
-          inherit tarball;
+          inherit tarball version;
           servicesFile = "deployment/DistributedDeployment/services.nix";
           networkFile = "deployment/DistributedDeployment/network.nix";
           distributionFile = "deployment/DistributedDeployment/distribution.nix";
@@ -52,8 +48,7 @@ let
         in
         disnixos.buildManifest {
           name = "disnix-stafftracker-nodejs-example";
-          version = builtins.readFile ./version;
-          inherit tarball;
+          inherit tarball version;
           servicesFile = "deployment/DistributedDeployment/services-with-caching.nix";
           networkFile = "deployment/DistributedDeployment/network.nix";
           distributionFile = "deployment/DistributedDeployment/distribution.nix";
@@ -64,52 +59,39 @@ let
     tests = let
       testScript = ''
         # Wait for a while and capture the output of the entry page
-        my $result = $test3->mustSucceed("sleep 30; curl --fail http://test1");
+        result = test3.succeed("sleep 30; curl --fail http://test1")
 
         # The entry page should contain my name :-)
 
-        if ($result =~ /Sander/) {
-            print "Entry page contains Sander!\n";
-        }
-        else {
-            die "Entry page should contain Sander!\n";
-        }
+        if "Sander" in result:
+            print("Entry page contains Sander!")
+        else:
+            raise Exception("Entry page should contain Sander!")
 
         # Start Firefox and take a screenshot
 
-        $test3->mustSucceed("firefox http://test1 &");
-        $test3->waitForWindow(qr/Firefox/);
-        $test3->mustSucceed("sleep 30");
-        $test3->screenshot("screen");
+        test3.succeed("firefox http://test1 &")
+        test3.wait_for_window("Firefox");
+        test3.succeed("sleep 30")
+        test3.screenshot("screen")
       '';
     in
     {
-      without_caching =
-        let
-          disnixos = import "${pkgs.disnixos}/share/disnixos/testing.nix" {
-            inherit nixpkgs;
-          };
-        in
-        disnixos.disnixTest {
-          name = "disnix-stafftracker-nodejs-example-tests";
-          inherit tarball;
-          manifest = builtins.getAttr (builtins.currentSystem) (build.without_caching);
-          networkFile = "deployment/DistributedDeployment/network.nix";
-          inherit testScript;
-        };
-      with_caching =
-        let
-          disnixos = import "${pkgs.disnixos}/share/disnixos/testing.nix" {
-            inherit nixpkgs;
-          };
-        in
-        disnixos.disnixTest {
-          name = "disnix-stafftracker-nodejs-example-tests";
-          inherit tarball;
-          manifest = builtins.getAttr (builtins.currentSystem) (build.with_caching);
-          networkFile = "deployment/DistributedDeployment/network.nix";
-          inherit testScript;
-        };
+      without_caching = disnixos.disnixTest {
+        name = "disnix-stafftracker-nodejs-example-tests";
+        inherit tarball;
+        manifest = builtins.getAttr (builtins.currentSystem) (build.without_caching);
+        networkFile = "deployment/DistributedDeployment/network.nix";
+        inherit testScript;
+      };
+
+      with_caching = disnixos.disnixTest {
+        name = "disnix-stafftracker-nodejs-example-tests";
+        inherit tarball;
+        manifest = builtins.getAttr (builtins.currentSystem) (build.with_caching);
+        networkFile = "deployment/DistributedDeployment/network.nix";
+        inherit testScript;
+      };
     };
   };
 in
